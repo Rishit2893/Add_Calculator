322b75dda459b8fc881dd9d8d72038ba4f1f3654
Rishit2893 <rishitkhadawala28@gmail.com>
2024-07-30 00:50:45 +0530

    simple string calculator with multiple negative no. exception

--- add_calculator.js
+++ add_calculator.js
@@ -1,25 +1,31 @@
-function add(numbers){
-    try{
-        let del=',';
-        if(numbers.startsWith("//")){
-            del=numbers[2];
+function add(numbers) {
+    try {
+        let del = ',';
+        if (numbers.startsWith("//")) {
+            del = numbers[2];           //taking delemiter from string
+            numbers = numbers.slice(4); // remove the custom delimiter part from the string
         }
-        const operands = numbers.split(RegExp(`${del}|\n`));// all numbers are stored seperately if seperators are delemiter specified or new line
-        if(del!=','){
-            operands.shift(); // remove the delemiter part from operands
-        }
-        let sum=0;
+        const operands = numbers.split(new RegExp(`${del}|\n`)); // all numbers are stored separately if separators are delimiter specified or new line
+        let sum = 0;
+        let negativeNumbers = [];
+
         operands.forEach(element => {
-            if(element<0){
-                throw new Error("Negative numbers not allowed: " + element);
+            const num = parseInt(element);
+            if (num < 0) {
+                negativeNumbers.push(num);   //if negative no. add to list
             }
-            sum+=parseInt(element); // each number is added 
+            sum += num; // each number is added
         });
+
+        if (negativeNumbers.length > 0) {
+            throw new Error("Negative numbers not allowed: " + negativeNumbers.join(', '));
+        }
+
         return sum;
-    }
-    catch(error){
+    } catch (error) {
         console.log(error);
     }
 }
-var input = "//;1\n2;3;-4;5";
+
+var input = "//;1\n2;3;-4;5;-6";
 console.log("Sum: " + add(input));
