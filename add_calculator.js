0afb606c7a8a54cc80ff64dca6b3604f0dcb97a6
Rishit2893 <rishitkhadawala28@gmail.com>
2024-07-30 00:45:45 +0530

    simple string calculator with negative no. exception

--- add_calculator.js
+++ add_calculator.js
@@ -1,17 +1,25 @@
 function add(numbers){
-    let del=',';
-    if(numbers.startsWith("//")){
-        del=numbers[2];
+    try{
+        let del=',';
+        if(numbers.startsWith("//")){
+            del=numbers[2];
+        }
+        const operands = numbers.split(RegExp(`${del}|\n`));// all numbers are stored seperately if seperators are delemiter specified or new line
+        if(del!=','){
+            operands.shift(); // remove the delemiter part from operands
+        }
+        let sum=0;
+        operands.forEach(element => {
+            if(element<0){
+                throw new Error("Negative numbers not allowed: " + element);
+            }
+            sum+=parseInt(element); // each number is added 
+        });
+        return sum;
     }
-    const operands = numbers.split(RegExp(`${del}|\n`));// all numbers are stored seperately if seperators are delemiter specified or new line
-    if(del!=','){
-        operands.shift(); // remove the delemiter part from operands
+    catch(error){
+        console.log(error);
     }
-    let sum=0;
-    operands.forEach(element => {
-        sum+=parseInt(element); // each number is added 
-    });
-    return sum;
 }
-var input = "//;1\n2;3;4;5";
+var input = "//;1\n2;3;-4;5";
 console.log("Sum: " + add(input));
