19a24eab0034db94fe03dd183fd229ef584b98a3
Rishit2893 <rishitkhadawala28@gmail.com>
2024-07-30 00:34:40 +0530

    simple string calculator with delemiters specified in string

--- add_calculator.js
+++ add_calculator.js
@@ -1,10 +1,17 @@
 function add(numbers){
-    const operands = numbers.split(/,|\n/);// all numbers are stored seperately if seperators are , or new line
+    let del=',';
+    if(numbers.startsWith("//")){
+        del=numbers[2];
+    }
+    const operands = numbers.split(RegExp(`${del}|\n`));// all numbers are stored seperately if seperators are delemiter specified or new line
+    if(del!=','){
+        operands.shift(); // remove the delemiter part from operands
+    }
     let sum=0;
     operands.forEach(element => {
         sum+=parseInt(element); // each number is added 
     });
     return sum;
 }
-var input = "1\n2,3,4\n5";
+var input = "//;1\n2;3;4;5";
 console.log("Sum: " + add(input));
